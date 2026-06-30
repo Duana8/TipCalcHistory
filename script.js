cdc24df3f16c9a6c0786e2b32f577b9df2c021fe
Diana <224145@kuzstu.ru>
2026-06-30 18:15:28 +0700

    v1.0

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('tip-form');
+const billInput = document.getElementById('bill-amount');
+const tipSelect = document.getElementById('tip-percentage');
+const history = document.getElementById('history-list');
+const empty_mes = document.getElementById('empty-message');
+
+// Нажатие кнопки
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // = "Не перезагружай страницу"
+
+    // Значения из полей
+    let bill = Number(billInput.value);
+    let percent = Number(tipSelect.value);
+
+    // Подсчет
+    let tip = Math.round((bill * percent),0);
+    
+    // Добавление строчку в историю
+    history.classList.remove('hidden');
+    empty_mes.classList.add('hidden');
+    const historyList = document.getElementById('history-list');
+    historyList.innerHTML += '<p>Заказ на ' + bill + ' — чаевые ' + tip + '</p>';
+});
