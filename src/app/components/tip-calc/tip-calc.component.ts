eb941decf05f74f8e56230171415ea3e7cc7a2a2
Диана <72574067+Duana8@users.noreply.github.com>
2026-07-22 13:38:31 +0700

    feat: changing template forms to reactive ones

--- src/app/components/tip-calc/tip-calc.component.ts
+++ src/app/components/tip-calc/tip-calc.component.ts
@@ -1,17 +1,19 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit, effect } from "@angular/core";
 import { TipCalcService } from "../../services/tip-calc/tip-calc.service";
-import { FormsModule } from "@angular/forms";
+import { FormBuilder, FormGroup, 
+    ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
     selector:'app-tip-calc',
     standalone: true,
-    imports: [FormsModule],
+    imports: [ReactiveFormsModule],
     templateUrl: './tip-calc.component.html',
     styleUrls: ['./tip-calc.component.scss']
 })
 
-export class TipCalcComponent {
+export class TipCalcComponent implements OnInit {
     protected tipService = inject(TipCalcService);
+    private readonly data = inject(FormBuilder);
 
     // + api валюты
      currencies= [
@@ -19,10 +21,33 @@ export class TipCalcComponent {
         {name: '$', value: 5},
         {name: '€', value: 8}
     ]
-    currency:string=this.currencies[0].name;
 
-    ngOnInit() { // не работает
-        if (this.tipService.currency) 
-            this.currencies[0].name; 
-    } 
+    protected tipForm: FormGroup = this.data.group({
+        bill: [this.tipService.bill, [Validators.required, Validators.min(10)]],
+        precent: [this.tipService.percent], 
+        currency: [this.tipService.currency]
+    });
+
+    constructor() {
+    // Следим за изменениями bill в сервисе
+        effect(() => {
+            this.tipForm.patchValue({ bill: this.tipService.bill }, { emitEvent: false });
+            
+            if (this.tipForm.valid) {
+                this.tipService.calcTip();
+            }
+        });
+    }
+
+    ngOnInit() {
+        // Авторасчет при изменениях в форме
+        this.tipForm.valueChanges.subscribe(values => {
+            this.tipService.percent = Number(values.precent) || 10;
+            this.tipService.currency = Number(values.currency) || 2;
+
+            if (this.tipForm.valid) {
+                this.tipService.calcTip();
+            }
+        });
+    }
 }
